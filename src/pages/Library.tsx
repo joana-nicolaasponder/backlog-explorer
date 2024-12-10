d02b71837d2fd0b4f58a625341f092b8211880a0
Joana NicolaasPonder <joanaponder@gmail.com>
2024-12-10 11:16:37 +0100

    add game populates with genres and platforms as expected

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import supabase from '../supabaseClient'
 import GameCard from '../components/GameCard'
-import AddGameModal from '../components/AddGameModal'
 import SearchBar from '../components/SearchBar'
 
 const Library = () => {
