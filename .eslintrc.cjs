f2aae3ab7133138c6a15535580e5490f9dda5770
Joana NicolaasPonder <joanaponder@gmail.com>
2025-02-04 11:15:14 +0100

    fix: update ESLint configuration and lint script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,31 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true, 
+    es2020: true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist/**/*',
+    'node_modules/**/*',
+    '.eslintrc.cjs',
+    'vite.config.ts',
+    'tests/setup.js'
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.test.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
