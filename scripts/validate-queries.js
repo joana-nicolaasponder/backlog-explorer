cdff8f100f958542000031c9b52a0c6fe5d84c5b
Joana NicolaasPonder <joanaponder@gmail.com>
2025-02-07 13:55:24 +0100

    fix: update glob import in validate-queries script

--- scripts/validate-queries.js
+++ scripts/validate-queries.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-import glob from 'glob';
+import { glob } from 'glob';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
